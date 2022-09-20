cd19a51cb22d7077b70e41d8c9abe009e41f8258
dzonic <nikolicnikola4466@gmail.com>
2022-09-20 18:34:32 +0200

    Can edit and add new users in admin panel

--- users/admin.py
+++ users/admin.py
@@ -16,4 +16,18 @@ class MyAdminAccounts(UserAdmin):
     readonly_fields = ['date_joined']
 
 
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('email', 'first_name', 'last_name','password1','password2', 'is_employee', 'is_employer', 'is_active')
+        }),
+    )
+
+
+    fieldsets = (
+        (None,{'fields':('email', 'first_name', 'last_name','password')}),
+        ('Permissions',{'fields':('is_staff','is_active','is_employee','is_employer')})
+    )
+
+
 admin.site.register(Account, MyAdminAccounts)
