c0cfb377b1109269bc8fd9f42a2e19056805434c
dzonic <nikolicnikola4466@gmail.com>
2022-09-20 19:32:01 +0200

    Listed added Jobs at index.html

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView,ListView
 
+from jobs.models import Job
+
 
 class HomeView(ListView):
     template_name = 'jobs/index.html'
     context_object_name = 'jobs'
+    model = Job
