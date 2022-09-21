6d5ecf3835bdbb17a129aece118b8dd194b0af96
dzonic <nikolicnikola4466@gmail.com>
2022-09-21 10:47:29 +0200

    Added Category model

--- jobs/models.py
+++ jobs/models.py
@@ -4,7 +4,17 @@ from django.template.defaultfilters import slugify
 from Job import settings
 
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(default=None, editable=False)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
 
 class Job(models.Model):
     title = models.CharField(max_length=300)
@@ -24,17 +34,14 @@ class Job(models.Model):
     publishing_date = models.DateField(auto_now_add=True)
     slug = models.SlugField(default=None, editable=False)
     employer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
-
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="jobs",default=1)
 
     def __str__(self):
-        return  self.title
+        return self.title
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Job, self).save(*args, **kwargs)
 
-
-
     class Meta:
         ordering = ('-id',)
-
