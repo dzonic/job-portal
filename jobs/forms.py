7a53c1faa5d9a13f477c1711c99ce98231c60309
dzonic <nikolicnikola4466@gmail.com>
2022-09-27 14:36:01 +0200

    Updated Job Post form

--- jobs/forms.py
+++ jobs/forms.py
@@ -6,4 +6,4 @@ class CreateJobForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = ['title', 'company', 'location', 'job_type', 'category', 'description']
-        widgets = {'job_type':forms.RadioSelect}
+        widgets = {'job_type': forms.RadioSelect}
--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, ListView, CreateView
 
 from jobs.models import Job, Category
@@ -17,9 +19,16 @@ class HomeView(ListView):
         return context
 
 
+@method_decorator(login_required(login_url='/'), name='dispatch')
 class CreateJobView(SuccessMessageMixin, CreateView):
     model = Job
     template_name = 'jobs/create-jobs.html'
     form_class = CreateJobForm
     success_url = '/'
     success_message = "Job has been posted!"
+
+    def form_valid(self, form):
+        job = form.save(commit=False)
+        job.employer = self.request.user
+        job.save()
+        return super(CreateJobView, self).form_valid(form)
